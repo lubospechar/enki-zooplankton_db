b548ecb4010227b933cc305dd1fd8d71d2744c04
Luboš Pechar <lubos.pechar@prirodou.cz>
2026-04-02 10:36:11 +0200

    Rename app to 'samples', add models, admin configuration, and integrate with project settings

--- samples/apps.py
+++ samples/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
-class ZoodbConfig(AppConfig):
-    name = _("samples")
+class SamplesConfig(AppConfig):
+    name = "samples"
+    verbose_name = _("Samples")
