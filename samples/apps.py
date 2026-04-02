1667449945f10141cb32dc331e01ab11ef516503
Luboš Pechar <lubos.pechar@prirodou.cz>
2026-04-02 10:26:40 +0200

    Rename 'zoodb' app to 'samples' and implement models for Location, Project, and Sample

---
+++ samples/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+class ZoodbConfig(AppConfig):
+    name = _("samples")
--- zoodb/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ZoodbConfig(AppConfig):
-    name = "zoodb"
